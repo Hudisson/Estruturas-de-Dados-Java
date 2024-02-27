e6a678ec4b955e3e42e4f057c2ca19246ca8acf4
Hudisson Xavier <hudissonxavier2018@gmail.com>
2024-02-26 22:19:48 -0300

    Add classe Fila

--- src/com/hudisson/estruturadados/fila/Fila.java
+++ src/com/hudisson/estruturadados/fila/Fila.java
@@ -4,10 +4,12 @@ import com.hudisson.estruturadados.base.EstruturaEstatica;
 
 public class Fila<TIPO> extends EstruturaEstatica<TIPO>{
 
+    /* Método construtor */
     public Fila(){
         super();
     }
 
+     /* Método construtor  parametrizado */
     public Fila(int capacidade){
         super(capacidade);
     }
